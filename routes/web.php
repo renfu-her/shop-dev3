fd84116db239e1b05ee4f908e6cf19c92c42314a
Patrick Her <renfu.her@gmail.com>
2024-12-31 09:36:21 +0800

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CaptchaController;
 
 Route::get('/', function () {
     return redirect()->route('admin.products.index');
 });
+
+// 驗證碼路由
+Route::get('/captcha', [CaptchaController::class, 'generate'])->name('captcha.generate');
+Route::post('/captcha/refresh', [CaptchaController::class, 'generate'])->name('captcha.refresh');
